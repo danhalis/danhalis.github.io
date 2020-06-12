42279934b1850130470803e936972f0fff1b7d56
DanHalis <danhalisly@gmail.com>
2020-06-12 12:17:45 -0700

    update home page

--- indexscript.js
+++ indexscript.js
@@ -1,5 +1,6 @@
 // General rules for all pages //
 document.body.style.maxWidth = window.innerWidth + "px";
+let firstHeight = null;
 
 $(document).ready(function()
 {
@@ -26,7 +27,22 @@ $(document).ready(function()
     $("#see-less-button").click(function()
     {
         $("#see-less-button").css("display", "none");
-        $("#profile-info").css("height", "160px");
+        $("#profile-info").css("height", firstHeight);
         $("#see-more-button").css("display", "flex");
     });
 });
+
+let belowLaptopScreen = window.matchMedia("(max-width: 1024px").matches;
+
+if (belowLaptopScreen) {
+    let profileInfo = document.getElementById("profile-info");
+    let profileHeading = document.getElementById("profile-heading");
+    let firstParagraph = document.getElementById("first-p");
+
+    firstHeight = parseFloat(window.getComputedStyle(profileInfo, null).getPropertyValue('padding-top'))
+                               + parseFloat(window.getComputedStyle(profileInfo, null).getPropertyValue('padding-bottom')) 
+                               + profileHeading.offsetHeight 
+                               + parseFloat(window.getComputedStyle(profileHeading, null).getPropertyValue('margin-bottom')) 
+                               + firstParagraph.offsetHeight + "px";
+    profileInfo.style.height = firstHeight;
+}
